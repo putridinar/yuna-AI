aae22573745279cc6cf67a8d89ac3c7c0c2be56a
daffadevhosting <endsae83@gmail.com>
2025-06-16 15:12:10 +0700

    test

--- assets/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCIdZ7XoPU85BI_L5kyTJAMucLG6Cbm8ks",
-  authDomain: "sahabat-yuna.firebaseapp.com",
-  projectId: "sahabat-yuna",
-  storageBucket: "sahabat-yuna.firebasestorage.app",
-  messagingSenderId: "202862709246",
-  appId: "1:202862709246:web:2b2ff80a506e21d9ccd8af"
-};
